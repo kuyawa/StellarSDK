77d98430b6523f1b7399a388cf0d0e5508697967
kuyawa <haxapp@gmail.com>
2018-01-23 13:11:38 -0400

    Initial Commit

---
+++ StellarSDK/StellarSDK.h
@@ -0,0 +1,19 @@
+//
+//  StellarSDK.h
+//  StellarSDK
+//
+//  Created by Laptop on 1/23/18.
+//  Copyright © 2018 Armonia. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for StellarSDK.
+FOUNDATION_EXPORT double StellarSDKVersionNumber;
+
+//! Project version string for StellarSDK.
+FOUNDATION_EXPORT const unsigned char StellarSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StellarSDK/PublicHeader.h>
+
+
