14e5254cfce577833675472fdd08b91b8aed088c
kuyawa <haxapp@gmail.com>
2018-02-14 17:52:56 -0400

    All platforms

--- StellarSDK/StellarSDK.h
+++ StellarSDK/StellarSDK.h
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Armonia. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for StellarSDK.
 FOUNDATION_EXPORT double StellarSDKVersionNumber;
